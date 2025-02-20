1ce50fc688ee69561ff8a5f81e9e707dd6d81fa8
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-02-20 20:01:28 +0600

    backend api for moving drag and drop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 app.use(cors());
@@ -25,6 +25,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const userCollection = client.db("taskFlowDB").collection("users");
+    const taskCollection = client.db("taskFlowDB").collection("tasks");
     app.post('/users',async(req,res)=>{
         const user = req.body;
         const query = { email: user.email };
@@ -35,6 +36,41 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result)
     })
+    app.post('/tasks',async(req,res)=>{
+        const task = req.body;
+        const result = await taskCollection.insertOne(task);
+        res.send(result)
+    })
+    app.get('/tasks/:email',async(req,res)=>{
+        const email = req.params.email;
+        const query = {email: email}
+        const data = await taskCollection.find(query).toArray();
+        res.send(data)
+    })
+    app.put('/tasks/:id', async (req, res) => {
+        const id = new ObjectId(req.params.id); 
+        const  {category}  = req.body;  
+  
+        try {
+          
+          const result = await taskCollection.updateOne(
+            { _id: new ObjectId(id) },  
+            { $set: { category: category } }  
+          );
+  
+          if (result.matchedCount === 0) {
+            return res.status(404).send("Task not found");
+          }
+  
+         
+          const updatedTask = await taskCollection.findOne({ _id: new ObjectId(id) });
+          res.json(updatedTask);
+        } catch (error) {
+          console.error("Error updating task:", error);
+          res.status(500).send("Server Error");
+        }
+      });
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
